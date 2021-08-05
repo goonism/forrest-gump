aa4548f4732c1046943ea46cd95c8b87f4414f5a
Karlos Santos <6723574+louisgv@users.noreply.github.com>
2021-08-05 16:50:02 -0400

    Add atdatabases

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,45 +1,23 @@
-import * as React from "react";
-import { Card, TextInput, Button } from "react-native-paper";
-import * as SQLite from "expo-sqlite";
-import {
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import Constants from "expo-constants";
+import connect, { sql } from "@databases/expo"
+import Constants from "expo-constants"
+import * as React from "react"
+import { StyleSheet, Text, View } from "react-native"
+import { Button, Card, TextInput } from "react-native-paper"
 
-function openDatabase() {
-  if (Platform.OS === "web") {
-    return {
-      transaction: () => {
-        return {
-          executeSql: () => {},
-        };
-      },
-    };
-  }
-
-  const db = SQLite.openDatabase("dreamyolo.db");
-  return db;
-}
-const db = openDatabase();
+const db = connect("dreams.db")
 
 function Items() {
-  const [items, setItems] = React.useState(null);
+  const [items, setItems] = React.useState(null)
 
   React.useEffect(() => {
-    db.transaction((tx) => {
-      tx.executeSql(`select * from dreams;`, null, (_, { rows }) =>
-        setItems(rows._array)
-      );
-    });
-  }, []);
+    db.tx(function* (transaction) {
+      const result = yield transaction.query(sql`select * from dreams;`)
+      setItems(result)
+    })
+  }, [])
 
   if (items === null || items.length === 0) {
-    return null;
+    return null
   }
 
   return (
@@ -49,47 +27,37 @@ function Items() {
         <Text>{description + " " + food + " " + bowel}</Text>
       ))}
     </View>
-  );
+  )
 }
 
 export default function App() {
-  const [dreamText, setDreamText] = React.useState("");
-  const [foodText, setFoodText] = React.useState("");
-  const [bowelText, setBowelText] = React.useState(0);
+  const [dreamText, setDreamText] = React.useState("")
+  const [foodText, setFoodText] = React.useState("")
+  const [bowelText, setBowelText] = React.useState(0)
 
-  const [forceUpdate, forceUpdateId] = useForceUpdate();
+  const { forceUpdate, forceUpdateId } = useForceUpdate()
 
   React.useEffect(() => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        "create table if not exists dreams (id integer primary key not null, description text, food text, bowel int);",
-        null,
-        null,
-        (txObj, error) => console.log("Creation Error ", error)
-      );
-    });
-  }, []);
+    db.tx(function* (transaction) {
+      transaction.query(
+        sql`create table if not exists dreams (id integer primary key not null, description text, food text, bowel int);`
+      )
+    })
+  }, [])
 
   const add = () => {
-    console.log("in add");
-    console.log("in add");
-    db.transaction(
-      (tx) => {
-        console.log("inside the transaction", dreamText, foodText, bowelText);
-        tx.executeSql(
-          "insert into dreams (description, food, bowel) values (?, ?, ?)",
-          [dreamText, foodText, bowelText],
-          null,
-          (txObj, error) => console.log("Error ", error)
-        );
-        tx.executeSql("select * from dreams", [], (_, { rows }) =>
-          console.log(JSON.stringify(rows))
-        );
-      },
-      null,
-      forceUpdate
-    );
-  };
+    db.tx(function* (transaction) {
+      console.log("inside the transaction", dreamText, foodText, bowelText)
+      yield transaction.query(
+        sql`insert into dreams (description, food, bowel) values (${dreamText}, ${foodText}, ${bowelText})`
+      )
+      const result = yield transaction.query(sql`select * from dreams`)
+
+      console.log(JSON.stringify(result))
+
+      forceUpdate()
+    })
+  }
 
   return (
     <View style={styles.container}>
@@ -106,7 +74,7 @@ export default function App() {
       />
       <TextInput
         label="How many bowel movement (number)"
-        value={bowelText}
+        value={bowelText.toString()}
         keyboardType={"numeric"}
         onChangeText={(t: string) => setBowelText(parseInt(t))}
       />
@@ -117,12 +85,15 @@ export default function App() {
         <Items key={`forceupdate-todo-${forceUpdateId}`} />
       </Card>
     </View>
-  );
+  )
 }
 
 function useForceUpdate() {
-  const [value, setValue] = React.useState(0);
-  return [() => setValue(value + 1), value];
+  const [value, setValue] = React.useState(0)
+  return {
+    forceUpdate: () => setValue(value + 1),
+    forceUpdateId: value
+  }
 }
 
 const styles = StyleSheet.create({
@@ -131,14 +102,14 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     paddingTop: Constants.statusBarHeight,
     backgroundColor: "#ecf0f1",
-    padding: 8,
+    padding: 8
   },
   sectionContainer: {
     marginBottom: 16,
-    marginHorizontal: 16,
+    marginHorizontal: 16
   },
   sectionHeading: {
     fontSize: 18,
-    marginBottom: 8,
-  },
-});
+    marginBottom: 8
+  }
+})
